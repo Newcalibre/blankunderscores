85f58021ed9e31071f24f6730330bcef164cca79
Charlie Lee <charlie@studiohybrid.com>
2015-04-17 13:00:19 +1000

    removed scripts

--- header.php
+++ header.php
@@ -18,9 +18,6 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
 <?php wp_head(); ?>
 </head>
 
